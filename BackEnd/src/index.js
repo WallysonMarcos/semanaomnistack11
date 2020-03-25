e2ceeed1713a910105f62554311c029a0799bcd4
Wallyson Silva <wallyson78@gmail.com>
2020-03-25 11:20:48 -0400

    Back-end the hero

---
+++ BackEnd/src/index.js
@@ -0,0 +1,19 @@
+﻿/**
+ * Rota : Recurso
+ * 
+ * Query params: Parâmetros nomeados enviados na rota após "?" (Filtro, Paginação)
+ * Route params: Parâmetro utilizado para identificar recurso
+ * Resquest body: Corpo da reuisição, utilizado para criar ou alterar dados
+ */
+
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
---
+++ BackEnd/src/routes.js
@@ -0,0 +1,16 @@
+﻿const express = require('express');
+const routes = express.Router();
+const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+
+routes.get('/ongs', ongController.list);
+routes.post('/ongs', ongController.create);
+
+routes.get('/profiles', profileController.list);
+
+routes.get('/incidents', incidentController.list);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents', incidentController.delete);
+
+module.exports = routes;
