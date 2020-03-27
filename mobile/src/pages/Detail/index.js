c82ef7d3480236056fdcc11fb25bf7aa2d26e310
Wallyson Silva <wallyson78@gmail.com>
2020-03-27 13:40:35 -0400

    Subindo projeto

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+import { View } from 'react-native';
+
+export default function Detail(){
+    return(
+        <View />
+    );
+}
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+import { View } from 'react-native';
+
+export default function Incidents(){
+    return(
+        <View />
+    );
+}
