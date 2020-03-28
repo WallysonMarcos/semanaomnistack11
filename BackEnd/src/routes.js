7c9168a98e86e16abd5c520c23f693230e415796
Wallyson Silva <wallyson78@gmail.com>
2020-03-28 14:09:16 -0400

    Front-end Be The Hero

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -3,14 +3,17 @@ const routes = express.Router();
 const ongController = require('./controllers/ongController');
 const incidentController = require('./controllers/incidentController');
 const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
 
 routes.get('/ongs', ongController.list);
 routes.post('/ongs', ongController.create);
 
-routes.get('/profiles', profileController.list);
+routes.get('/profile', profileController.list);
+
+routes.post('/sessions', sessionController.create);
 
 routes.get('/incidents', incidentController.list);
 routes.post('/incidents', incidentController.create);
-routes.delete('/incidents', incidentController.delete);
+routes.delete('/incidents/:id', incidentController.delete);
 
 module.exports = routes;
